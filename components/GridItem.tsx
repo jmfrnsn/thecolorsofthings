d42f6bcf6576682ad3bf4991fe91f92a928a2f5c
jmfrnsn <117957262+jmfrnsn@users.noreply.github.com>
2025-02-13 21:09:47 -0500

    Update GridItem.tsx

--- components/GridItem.tsx
+++ components/GridItem.tsx
@@ -50,7 +50,7 @@ export function GridItem({ pixelatedSrc, unpixelatedSrc, alt, title }: GridItemT
           )}
         </div>
       </div>
-      <div className={`absolute bottom-0 left-0 right-0 pb-4 px-8 transform transition-transform duration-300 ${
+      <div className={`absolute bottom-0 left-0 right-0 pb-6 px-8 transform transition-transform duration-300 ${
           isClicked ? "translate-y-0" : "translate-y-full"}`}>
         <h2
           className="text-center text-xs tracking-widest uppercase font-light text-[#BBB5AA]"
